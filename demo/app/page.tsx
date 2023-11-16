81dd772a36377ad31190ebbb6c6a11e255c19b39
Grim <grim.e.kalman@gmail.com>
2023-11-16 16:20:21 +0100

    Add fetch call and some basic useStates

---
+++ demo/app/page.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import { MeiliSearch } from 'meilisearch'
+import { use, useState } from 'react'
+
+type Person= {
+  id: string;
+  name: string;
+  balance: string;
+}
+
+export default function Home() {
+  const [search, setSearch] = useState('');
+  const [people, setPeople] = useState<Person[]>([]);
+
+  const client = new MeiliSearch({
+    host: 'https://ms-23da8d068b58-6238.lon.meilisearch.io',
+    apiKey: '67259f4c8103fce551aacb916f40a9acf41ae6cd'
+  })
+  client.index('people').search().then((res) => console.log(res))
+
+  return (
+    <main>
+      <h1>Find the person!</h1>
+      <form>
+        <input name="query" />
+        <button type="submit">Search</button>
+      </form>
+    </main>
+  )
+}
